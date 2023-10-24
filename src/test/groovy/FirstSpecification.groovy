d8eb1ae83d7d014e77e25e39ef4fb0caa52c0aaf
Betânia Santos <131388308+betaniasan@users.noreply.github.com>
2023-10-25 00:25:50 +0100

    Passo 11.4 Assertions Exceptions

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -22,4 +22,13 @@ class FirstSpecification extends Specification {
         then:
         list == [2, 3, 4]
     }
+    def "Should get an index out of bounds when removing a non-existent item"() {
+        given:
+        def list = [1, 2, 3, 4]
+        when:
+        list.remove(20)
+        then:
+        thrown(IndexOutOfBoundsException)
+        list.size() == 4
+    }
 }
